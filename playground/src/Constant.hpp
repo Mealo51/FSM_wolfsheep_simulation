f24aedea67540fb58ab36ecec27e08277584fcd5
Mealo <meathanol51@gmail.com>
2026-02-22 14:27:51 +0100

    tile, constants and related entity change

--- playground/src/Constant.hpp
+++ playground/src/Constant.hpp
@@ -1,7 +1,14 @@
 //Constants.h
 #pragma once
 
-//config
+//window config
 constexpr int WINDOW_WIDTH = 1024;
 constexpr int WINDOW_HEIGHT = 1024;
 constexpr int fps_lim = 60;
+
+// "tile" size
+float tile_len = 20.f;
+
+//entities config
+float sheep_radius = 15.f;
+float wolf_radius = 20.f;
--- playground/src/entities.cpp
+++ playground/src/entities.cpp
@@ -2,16 +2,17 @@
 
 #include "entities.hpp"
 #include "common.hpp"
+#include "Constant.hpp"
 
 constexpr Color NON_COLLIDING_COLOR = LIME;
 
 sheep::sheep()
 {
-	HP = 100;
-	fullness = 0;
-	reproduce_cd = 20;
-	detection_radius = 4;
-	speed = 10;
+	HP = 100.f;
+	fullness = 0.f;
+	reproduce_cd = 20.f;
+	speed = 1.f * tile_len;
+	detection_radius = 4.f * tile_len;
 	wolfNearby = false;
 	position = { Random::rangef(0.f, 1024.f),Random::rangef(0.f, 1024.f) };
 	velocity = { 0.f,0.f };
@@ -26,14 +27,14 @@ void sheep::update(float dt)
 
 void sheep::render() const
 {
-	DrawCircleV(position, 15, WHITE);
+	DrawCircleV(position, sheep_radius, WHITE);
 }
 
 wolf::wolf()
 {
 	hunger = 0;
-	speed = 1.5f;
-	detection_radius = 2;
+	speed = 1.5f * tile_len;
+	detection_radius = 2.f * tile_len;
 	position = { 0,0 };
 	velocity = { 0,0 };
 	acceleration = { 0,0 };
