98bb51f1f92a5537b56e84a8c33a1d68baa5d1e9
Mealo <meathanol51@gmail.com>
2026-02-23 14:29:01 +0100

    tile update

--- playground/src/Constant.hpp
+++ playground/src/Constant.hpp
@@ -8,6 +8,8 @@ constexpr int fps_lim = 60;
 
 // "tile" size
 static float tile_len = 20.f;
+static int tile_x = 1024 / tile_len;
+static int tile_y = 1024 / tile_len;
 
 //entities config
 static float sheep_radius = 15.f;
